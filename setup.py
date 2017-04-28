8609ce8166f61091067c0311e863ffbfa904938d
Lemon Lam <almk@rmntn.net>
2017-04-29 04:26:06 +0800

    Project: Add Py 3.6 indicator for PyPI

--- setup.py
+++ setup.py
@@ -50,7 +50,8 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3 :: Only'
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6'
     ],
 
     # What does your project relate to?
@@ -68,7 +69,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['telethon', 'click'],
+    install_requires=['telethon', 'click', 'PyYAML'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
@@ -99,5 +100,5 @@ setup(
         'console_scripts': [
             'pytgasu=pytgasu:cli',
         ],
-    },
+    }
 )
